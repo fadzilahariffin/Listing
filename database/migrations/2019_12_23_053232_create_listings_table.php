d793d00e31bb81b6ec280f76585df8681ccfa99b
Fadzilah Ariffin <fadzilah9595@gmail.com>
2019-12-23 13:47:16 +0800

    migrate users&listing and seed users

---
+++ database/migrations/2019_12_23_053232_create_listings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('list_name');
+            $table->string('address');
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->unsignedBigInteger('submitter_id');
+            $table->timestamps();
+
+            $table->foreign('submitter_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+}
