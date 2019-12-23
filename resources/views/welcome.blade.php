e4eae02e13de8c32fe100c5e88901aefffe65084
Fadzilah Ariffin <fadzilah9595@gmail.com>
2019-12-23 13:28:00 +0800

    add release/staging branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Listing
+                    Welcome to Listing System
                 </div>
             </div>
         </div>
